aae8af107dc5ac3af44eca618a40cc2464e3080a
Campbell Hutcheson <nchutcheson@gmail.com>
2024-04-20 15:36:22 -0700

    updated prompts

--- src/writer.py
+++ src/writer.py
@@ -1,1 +1,39 @@
 # functions for patent generation
+from typing import List, Dict
+
+from utils import generator
+from writer_prompts import format_abstract_prompt, format_plan_specification_prompt, format_specification_prompt, format_claims_prompt
+
+
+def generate_patent(invention_description, prior_art):
+    patent = generate_abstract(invention_description, prior_art)
+
+    specification_plan = plan_specification(patent, invention_description, prior_art)
+
+    for section in specification_plan:
+        next_section = generate_specification(section, patent, invention_description, prior_art)
+        patent += next_section
+
+    claims = generate_claims(patent, invention_description, prior_art)
+
+    return patent + claims
+
+def generate_abstract(invention_description: Dict[str, str], prior_art: List[Dict[str, str]]) -> str:
+    abstract_prompt = format_abstract_prompt(invention_description, prior_art)
+    abstract = generator(abstract_prompt)
+    return abstract
+
+def plan_specification(abstract, invention_description, prior_art):
+    plan_specification_prompt = format_plan_specification_prompt(abstract, invention_description, prior_art)
+    plan_specification = generator(plan_specification_prompt)
+    return plan_specification
+
+def generate_specification(section, patent, invention_description, prior_art):
+    specification_prompt = format_specification_prompt(section, patent, invention_description, prior_art)
+    specification = generator(specification_prompt)
+    return specification
+
+def generate_claims(specification, invention_description, prior_art):
+    claims_prompt = format_claims_prompt(specification, invention_description, prior_art)
+    claims = generator(claims_prompt)
+    return claims
---
+++ src/writer_prompts.py
@@ -0,0 +1,73 @@
+def format_abstract_prompt(invention_disclosure, prior_art):
+    return f"""
+    You have been asked to write an abstract for a patent application.
+
+    The invention disclosure is as follows:
+    {invention_disclosure}
+
+    The prior art is as follows:
+    {prior_art}
+
+    Write an abstract for this patent application.
+    Do not return any text other than the abstract.
+    """
+
+def format_plan_specification_prompt(abstract, invention_disclosure, prior_art):
+    return f"""
+    You have been asked to write a patent application for the following invention:
+
+    Abstract:
+    {abstract}
+
+    Invention Disclosure:
+    {invention_disclosure}
+
+    Prior Art:
+    {prior_art}
+
+    You are to write a plan for the patent specification. Each of these sections will then be written separately.
+
+    Return your response as a list of strings, where each string is a section of the plan.
+    """
+
+def format_specification_prompt(section, patent, invention_disclosure, prior_art):
+    return f"""
+    You have been asked to write a patent application for the following invention:
+
+    Invention Disclosure:
+    {invention_disclosure}
+
+    Prior Art:
+    {prior_art}
+
+    You are to write the following section of the patent specification:
+    {section}
+
+    So far, the patent application looks like this:
+    {patent}
+
+    Write the section of the patent specification.
+
+    Do not return any text other than the section of the patent specification.
+    """
+
+def format_claims_prompt(patent, invention_disclosure, prior_art):
+    return f"""
+    You have been asked to write a patent application for the following invention:
+
+    Invention Disclosure:
+    {invention_disclosure}
+
+    Prior Art:
+    {prior_art}
+
+    You are to write the claims section of the patent specification.
+
+    So far, the patent application looks like this:
+
+    {patent}
+
+    Write the claims section of the patent specification.
+
+    Do not return any text other than the claims.
+    """
