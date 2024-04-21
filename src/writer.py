3ac8b22271bd5318c87356a4ae8e045d072d44ad
Campbell Hutcheson <nchutcheson@gmail.com>
2024-04-20 17:22:23 -0700

    updated output

--- src/writer.py
+++ src/writer.py
@@ -4,24 +4,29 @@ import json
 from utils import generator
 from writer_prompts import format_abstract_prompt, format_plan_specification_prompt, format_specification_prompt, format_claims_prompt
 
+def patent_to_markdown(patent):
+    return "".join(value in patent.values())
+
 
 def generate_patent(invention_description, prior_art):
     patent = {}
 
     abstract = generate_abstract(invention_description, prior_art)
     patent["abstract"] = abstract
-        
+    
+    """
     specification_plan = plan_specification(patent, invention_description, prior_art)
     patent["specification"] = ""
 
     for section in specification_plan:
         next_section = generate_specification(section, patent, invention_description, prior_art)
         patent["specification"] += next_section
+    """
 
     claims = generate_claims(patent, invention_description, prior_art)
     patent["claims"] = claims
 
-    return json.dumps(patent, indent=4)
+    return patent
 
 def generate_abstract(invention_description, prior_art):
     abstract_prompt = format_abstract_prompt(invention_description, prior_art)
@@ -43,7 +48,7 @@ def generate_claims(specification, invention_description, prior_art):
     claims = generator(claims_prompt)
     return claims
 
-
+"""
 with open("../samples/disc_sample_1.json", "r") as f:
     invention_description = json.load(f)
 
@@ -54,3 +59,4 @@ generated_patent = generate_patent(invention_description, prior_art)
 
 with open("generated_patent.txt", "w") as f:
     f.write(generated_patent)
+"""
